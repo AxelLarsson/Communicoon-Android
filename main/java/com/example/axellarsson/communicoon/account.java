420c5fd438a9c7105eb82102f1138fcbd91e8552
AxelLarsson <45592556+AxelLarsson@users.noreply.github.com>
2018-12-11 14:55:36 +0100

    Tab update
    The app now contains tabs for the three different activities

---
+++ main/java/com/example/axellarsson/communicoon/account.java
@@ -0,0 +1,21 @@
+package com.example.axellarsson.communicoon;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class account extends Fragment {
+    private static final String tag = "Account";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.account, container, false);
+
+        return view;
+    }
+}
---
+++ main/java/com/example/axellarsson/communicoon/report.java
@@ -0,0 +1,24 @@
+package com.example.axellarsson.communicoon;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class report extends Fragment {
+    private static final String tag = "Report";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.report, container, false);
+
+
+
+        return view;
+    }
+}
