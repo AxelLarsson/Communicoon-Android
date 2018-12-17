d3d35f728c5135bdf174a8ad950cb43212a2061a
AxelLarsson <45592556+AxelLarsson@users.noreply.github.com>
2018-12-17 16:18:46 +0100

    Testing with connection to db

--- main/java/com/example/axellarsson/communicoon/archive.java
+++ main/java/com/example/axellarsson/communicoon/archive.java
@@ -7,15 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class archive extends Fragment {
     private static final String tag = "Archive";
+
+    ListView lv;
+    ArrayAdapter<String> adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.archive, container, false);
 
+
+        lv = (ListView) view.findViewById(R.id.archiveListView);
         return view;
     }
 }
--- main/java/com/example/axellarsson/communicoon/report.java
+++ main/java/com/example/axellarsson/communicoon/report.java
@@ -1,24 +1,134 @@
 package com.example.axellarsson.communicoon;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class report extends Fragment {
-    private static final String tag = "Report";
+    EditText Desc;
+    Button Submit;
+
+    ImageView imageViewCamera;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.report, container, false);
 
+        Button btnCamera = (Button) view.findViewById(R.id.btnCamera);
+        imageViewCamera = (ImageView) view.findViewById(R.id.imageViewCamera);
 
+        Desc = (EditText) view.findViewById(R.id.editText4);
+
+        Submit = view.findViewById(R.id.submit);
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Description = Desc.getText().toString();
+
+                BackgroundWorker bw = new BackgroundWorker(getActivity());
+                bw.execute(Description);
+            }
+        });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageViewCamera.setImageBitmap(bitmap);
+    }
+}
+
+class BackgroundWorker extends AsyncTask<String, Void, Void> {
+
+    Context context;
+
+    public BackgroundWorker(Context c){
+        context = c;
+    }
+
+    @Override
+    protected Void doInBackground(String...params) {
+        String server = "http://communicoon.com/app/post/test.php";
+
+        try {
+            String Description = params[0];
+
+            URL url = new URL(server);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            String postData = URLEncoder.encode("description", "UTF-8")+"="+URLEncoder.encode(Description, "UTF-8");
+            bufferedWriter.write(postData);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+
+            httpURLConnection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void...values) {
+        super.onProgressUpdate(values);
     }
 }
